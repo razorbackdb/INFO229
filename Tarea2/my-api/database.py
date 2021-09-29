b65fae3d25748424c34a3eabb4fb7a0c0c85610c
Carlos Leyan <razorbackdb@gmail.com>
2021-09-29 14:57:08 -0300

    Tarea2 1/2

--- Tarea2/my-api/database.py
+++ Tarea2/my-api/database.py
@@ -5,8 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://my-api:my-api-password@127.0.0.1:3306/tutorial1"
-SQLALCHEMY_DATABASE_URL = 'sqlite:///' + os.path.join(basedir, 'Sun.sqlite')
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@127.0.0.1:3306/INFO229"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
