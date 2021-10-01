872b83b73043c110c36f4b6d924c1fc43625a001
Carlos Leyan <razorbackdb@gmail.com>
2021-10-01 14:26:45 -0300

    Tarea2 2/2

---
+++ Tarea2/app/my-api/database.py
@@ -0,0 +1,14 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:root@db:3306/api"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
